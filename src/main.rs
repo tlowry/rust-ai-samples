e68bc84e4549e25e0dab70ae1eb33626ebc7aab2
Tony Lowry <tonylowryie@gmail.com>
2018-10-18 17:26:00 +0100

    Added ndarray, plays nice with std vectors.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 extern crate dataplotlib;
+extern crate ndarray;
 
 use dataplotlib::util::{linspace, zip2};
 use dataplotlib::plotbuilder::PlotBuilder2D;
 use dataplotlib::plotter::Plotter;
 use std::f64::consts::E;
 
+use ndarray::Array;
+
 fn main() {
     // Vector of 500 evenly spaced floats ranging from -10 to 10
     let x = linspace(-10, 10, 500);
-    draw(x, &sigmoid);
+
+    let input = Array::from_vec(x.clone());
+    let weights = Array::from_vec(x.clone());
+    let bb = input.dot(&weights);
+
+    println!("XX{:?}",bb);
+
+    draw(&x, &tanh);
 }
 
 // Sigmoid function
@@ -16,8 +26,13 @@ fn sigmoid(input: f64) -> f64 {
     return 1.0 / (1.0 + E.powf(-input));
 }
 
+// tanh function just use standard library
+fn tanh(input: f64) -> f64 {
+    return  input.tanh();
+}
+
 // Take a vector of floats and transform function and create a visual 2d graph of the result
-fn draw(x: Vec<f64>, f: &Fn(f64) -> f64) {
+fn draw(x: &Vec<f64>, f: &Fn(f64) -> f64) {
     let fx = x.iter().map(|x| f(*x)).collect();
     let results = zip2(&x, &fx);
 
